cec53cb0f7d5c0d2d148d652b2cf890e63db8dac
William Reynolds <williamareynolds@icloud.com>
2020-01-14 06:33:53 +0000

    Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class","2048":"Method"},"rows":[{"id":0,"kind":128,"name":"TestClass","url":"classes/testclass.html","classes":"tsd-kind-class"},{"id":1,"kind":2048,"name":"hello","url":"classes/testclass.html#hello","classes":"tsd-kind-method tsd-parent-kind-class","parent":"TestClass"}]};
